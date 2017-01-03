f40b767ec8f59967a82283c331556f14cad3b6d6
Arjun Raj <rajarju@gmail.com>
2017-01-03 12:35:00 +0530

    Render Method How to return html

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 class App extends React.Component {
 
   render() {
-    return <h1 className="title"> Hello World </h1>
+    return (
+      <div className="wrapper">
+        <h1 className="title"> Hello World </h1>
+        <b>Sub Head</b>
+      </div>
+    );
   };
 
 };
 
-
-// const App = () =>  <h1 className="title"> Hello World </h1>;
 
 export default App;
