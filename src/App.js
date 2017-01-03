11d1fe1e04192efdb9e53ce6a99b9cdf6c0807be
Arjun Raj <rajarju@gmail.com>
2017-01-03 12:54:18 +0530

    States in React components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      titleTxt : 'Hello World',
+      subHead : 'This is a Sub Head'
+    };
+  }
+
+
+  update(e) {
+    this.setState({titleTxt : e.target.value });
+  }
+
   render() {
-    let title = this.props.appTitle;
     return (
       <div className="wrapper">
-        <h1 className="title"> {title} </h1>
-        <b>Sub Head</b>
+        <h1 className="title"> {this.state.titleTxt} </h1>
+        <b>{this.state.subHead}</b>
+        <div>
+          <input type="text" onChange={this.update.bind(this)} />
+        </div>
       </div>
     );
-  };
-};
-
-App.propTypes = {
-  appTitle : React.PropTypes.string,
-  counter : React.PropTypes.number.isRequired
-};
-
-
-App.defaultProps = {
-  appTitle : 'Default Title',
-  counter : 2
+  }
 };
 
 export default App;
