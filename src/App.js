31711b8213b15177ceaba8e335bff296ecdc4c68
Arjun Raj <rajarju@gmail.com>
2017-01-03 12:42:36 +0530

    Set Properties for Components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
 class App extends React.Component {
-
   render() {
+    let title = this.props.appTitle;
     return (
       <div className="wrapper">
-        <h1 className="title"> Hello World </h1>
+        <h1 className="title"> {title} </h1>
         <b>Sub Head</b>
       </div>
     );
   };
-
 };
 
+App.propTypes = {
+  appTitle : React.PropTypes.string,
+  counter : React.PropTypes.number.isRequired
+};
+
+
+App.defaultProps = {
+  appTitle : 'Default Title',
+  counter : 2
+};
 
 export default App;
