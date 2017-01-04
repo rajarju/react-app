e78652bc12bfb9302660533f9a738fdee0f42d6c
Arjun Raj <rajarju@gmail.com>
2017-01-04 15:01:00 +0530

    Using Refs in components

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ class App extends React.Component {
 
   update(e) {
     this.setState({
-      a : ReactDOM.findDOMNode(this.a).value,
-      b : this.refs.b.value
+      a : this.a.refs.input.value,
+      b : this.b.refs.input.value
     })
   }
 
@@ -24,7 +24,7 @@ class App extends React.Component {
         <Input ref={ component => this.a = component }  update={this.update.bind(this)} />
         {this.state.a}
         <hr />
-        <input ref="b" type="text" onChange={this.update.bind(this)} />
+        <Input ref={ component => this.b = component }  update={this.update.bind(this)} />
         {this.state.b}
       </div>
     );
@@ -33,7 +33,11 @@ class App extends React.Component {
 
 class Input extends React.Component {
   render() {
-    return <input type="text" onChange={this.props.update} />
+    return (
+      <div>
+        <input ref="input" type="text" onChange={this.props.update} />
+      </div>
+    )
   }
 }
 
