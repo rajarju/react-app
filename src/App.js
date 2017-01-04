4a19d52893c8e73b0d071cbfcdfc0e8ea098fd04
Arjun Raj <rajarju@gmail.com>
2017-01-04 14:47:05 +0530

    Differentiate with refs

--- src/App.js
+++ src/App.js
@@ -5,21 +5,26 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      a : ''
+      a : '',
+      b : ''
     };
   }
 
   update(e) {
     this.setState({
-      a : e.target.value
+      a : this.refs.a.value,
+      b : this.refs.b.value
     })
   }
 
   render() {
     return (
       <div>
-        <input type="text" onChange={this.update.bind(this)} />
+        <input ref="a" type="text" onChange={this.update.bind(this)} />
         {this.state.a}
+        <hr />
+        <input ref="b" type="text" onChange={this.update.bind(this)} />
+        {this.state.b}
       </div>
     );
   }
