7fda24d530904c1a27d04baf01f1e22636e85ad6
Arjun Raj <rajarju@gmail.com>
2017-01-04 14:52:09 +0530

    Using refs callback

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
 
   update(e) {
     this.setState({
-      a : this.refs.a.value,
+      a : this.a.value,
       b : this.refs.b.value
     })
   }
@@ -20,7 +20,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <input ref="a" type="text" onChange={this.update.bind(this)} />
+        <input ref={ node => this.a = node } type="text" onChange={this.update.bind(this)} />
         {this.state.a}
         <hr />
         <input ref="b" type="text" onChange={this.update.bind(this)} />
