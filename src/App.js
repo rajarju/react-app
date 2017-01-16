75eae48f72de480519e3bcc3df83a16787e14d07
Arjun Raj <rajarju@gmail.com>
2017-01-16 14:17:13 +0530

    Lesson 19 Using clone to access child element.

--- src/App.js
+++ src/App.js
@@ -4,24 +4,44 @@ class App extends React.Component {
 
   render() {
     return (
-      <Parent>
-        <div className="childA"></div>
-        <div className="childB"></div>
-      </Parent>
+      <Buttons>
+        <button value="A">A</button>
+        <button value="B">B</button>
+        <button value="C">C</button>
+      </Buttons>
     );
   }
 };
 
-class Parent extends React.Component {
-  render() {
-    // let items = React.Children.map(this.props.children, child => child);
-    let items = React.Children.toArray(this.props.children);
-    // this.props.children.map( child => child )
-    // React.Children.forEach
-    // React.Children.only
-    console.log(items);
-    return null;
+class Buttons extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selected : 'None'
+    };
   }
+
+  selectItem(selected) {
+    this.setState({selected});
+  }
+
+  render() {
+
+    let fn = child =>
+      React.cloneElement(child, {
+        onClick : this.selectItem.bind(this, child.props.value)
+      });
+
+    let items = React.Children.map(this.props.children, fn);
+    return (
+      <div>
+        <h2>You have selected {this.state.selected}</h2>
+        {items}
+      </div>
+    );
+  }
+
 }
+
 
 export default App;
